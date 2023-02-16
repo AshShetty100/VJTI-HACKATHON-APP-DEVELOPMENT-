dad549852fb8acad0db22bff3e2293a7b5c60d2f
itsomkar30 <122784079+itsomkar30@users.noreply.github.com>
2023-02-16 14:43:56 +0530

    added

---
+++ AlarmManager/app/src/main/java/com/hellfire/alarmmanager/MedicineNotification.java
@@ -0,0 +1,14 @@
+package com.hellfire.alarmmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MedicineNotification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine_notification);
+    }
+}
